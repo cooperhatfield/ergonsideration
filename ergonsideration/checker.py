586140602b57817a4047fdf5796f86baacd04f1c
cooperhatfield <47254427+cooperhatfield@users.noreply.github.com>
2022-09-22 19:25:16 -0700

    Create and populate checker.py
    Class template for checkers

---
+++ ergonsideration/checker.py
@@ -0,0 +1,9 @@
+class Checker:
+	def is_running(self) -> bool:
+		raise NotImplementedError()
+
+	def get_status(self) -> bool:
+		raise NotImplementedError()
+
+	def timeout_time(self) -> int:
+		return 100 #ms
