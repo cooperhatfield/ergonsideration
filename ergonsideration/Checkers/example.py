91751a153558b43ea2ed6dd79a45c428e1790985
cooperhatfield <47254427+cooperhatfield@users.noreply.github.com>
2022-09-28 14:11:01 -0700

    Updated the example checker

--- ergonsideration/Checkers/example.py
+++ ergonsideration/Checkers/example.py
@@ -1,10 +1,10 @@
 from ergonsideration.checker import Checker
 
 class Example(Checker):
-	def is_running() -> bool:
+	def is_running(self) -> bool:
 		return True
 
-	def get_busy_status() -> bool:
+	def get_busy_status(self) -> bool:
 		return False
 
 	
