2db1fb738618188fa581209700ad23ba7583c365
cooperhatfield <47254427+cooperhatfield@users.noreply.github.com>
2022-10-31 11:48:46 -0700

    Renamed the example checker

---
+++ ergonsideration/Checkers/example_checker.py
@@ -0,0 +1,10 @@
+from ergonsideration.checker import Checker
+
+class ExampleChecker(Checker):
+	def is_running(self) -> bool:
+		return True
+
+	def get_busy_status(self, busy) -> bool:
+		busy = True
+
+	
