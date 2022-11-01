1ff4fedde24354723fd4e4b5f48d4029a18e51ba
cooperhatfield <47254427+cooperhatfield@users.noreply.github.com>
2022-11-01 13:06:46 -0700

    Added task logic to re-schedule tasks

--- ergonsideration/task.py
+++ ergonsideration/task.py
@@ -1,6 +1,7 @@
 import multiprocessing
 
 from checker import Checker
+from calendar import Calendar
 import notify
 
 class Task:
@@ -8,12 +9,15 @@ class Task:
 		self.notification_config = notification_config
 		self.schedule_config = schedule_config
 		self.checkers = checkers
+		self.calendar = Calendar()
 
 	def run_task(self):
 		user_is_busy = self.is_busy()
 		if not user_is_busy:
 			notify.send_notification(self.notification_config)
-		# re-schedule task here
+		# check notification button press for snooze or what not
+		delay = self.schedule_config['interval']
+		self.calendar.register_task(delay, self.run_task)
 
 	def is_busy(self):
 		busy = multiprocessing.Value(bool, False)
